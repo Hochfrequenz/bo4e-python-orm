f9e498de5c6a38d52d17e800ba9deff5e50506dc
DeltaDaniel <daniel.dernbach@hochfrequenz.de>
2023-09-26 15:20:21 +0200

    added Geschaeftspartner minimal version

--- db/base.py
+++ db/base.py
@@ -6,3 +6,4 @@ It imports all database models to be able to create all related tables
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from db.base_class import Base, mapper_registry  # noqa
+from src.bo4e.bo.geschaeftspartner import Geschaeftspartner
--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from db.auxiliary import get_url
-from bo4e.base import Base
+from db.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
