213da5425d958e7bd6eb3965be3b21e7c9ba6522
DeltaDaniel <daniel.dernbach@hochfrequenz.de>
2024-01-10 11:46:58 +0100

    fixed minor issue

--- src/borm/testing.py
+++ src/borm/testing.py
@@ -2,14 +2,14 @@ from sqlalchemy import create_engine
 from sqlmodel import Session
 
 from borm.db.postgresql_db.auxiliary import get_url
-from borm.models import Geschaeftspartner
 from borm.models.bo.angebot import Angebot
+from borm.models.bo.geschaeftspartner import Geschaeftspartner
 
 engine = create_engine(get_url(), echo=True)
 
 
 with Session(engine) as session:
-    geschaeftspartner = Geschaeftspartner(name1="Test")
-    angebot = Angebot(angebotsnummer="125", angebotsgeber=geschaeftspartner)
+    # geschaeftspartner = Geschaeftspartner(name1="Test")
+    angebot = Angebot(angebotsnummer="125")  # , angebotsgeber=geschaeftspartner)
     session.add(angebot)
     session.commit()
